05b99925823c4301c2c302666201bdda8c9223f8
Evaly Adam <152657385+Evalyadam@users.noreply.github.com>
2024-03-09 12:08:02 +0200

    Update HumanBodyInPython.py

--- HumanBodyInPython.py
+++ HumanBodyInPython.py
@@ -13,23 +13,15 @@ gender = random.choice(["Female", "Male"])
 
 
 class Human:
-        def __init__(self, name, sex, reactiontime):
-            self.name = name
-            self.sex = sex
-            self.reactiontime = reactiontime
-
+    def __init__(self, name, sex, reactiontime):
+        self.name = name
+        self.sex = sex
+        self.reactiontime = reactiontime
+    
     def __str__(self):
-        return f"{self.name}, {self.sex}, {self.reactiontime} s"
+        print(f"{self.name}, {self.sex}, {self.reactiontime} s")
 
-    def set_name(self, new_name):
-        self.name = new_name
 
-    def set_sex(self, new_sex):
-        self.sex = new_sex
-
-    def set_reaction_time(self, new_rt):
-        self.reactiontime = new_rt
-
-human = Human(name=name_options[randomnameselect], gender=gender)
+human = Human(name=name_options[randomnameselect], sex=gender, reactiontime=random.Random())
 
 
